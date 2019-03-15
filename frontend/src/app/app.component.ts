52b2a0ad18cd8d8b335be71781a5f868cde12e3e
prizraksarvar <sarkhas@mail.ru>
2019-03-15 15:06:50 +0500

    Create app.component.ts

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AutorizationService} from "./autorization/autorization.service";
+import {Subscription} from "rxjs";
+import {User} from "./entities/User";
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+    title = 'Angular admin';
+    user: User;
+    subscriptions:Subscription[];
+    autorizationService: AutorizationService;
+
+    constructor(autorizationService: AutorizationService) {
+        this.autorizationService = autorizationService;
+        this.subscriptions = [];
+        let state = autorizationService.getState();
+        this.user = null;
+        this.subscriptions.push(state.user$.subscribe((value:User)=>{this.user = value}));
+    }
+
+    logout() {
+        this.autorizationService.logout();
+    }
+
+    ngOnInit() {
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach((subsciption: Subscription, index: number, array: Subscription[]) => {
+            subsciption.unsubscribe();
+        });
+    }
+}
