6998a4e4243f26d99a1dc943cb5245d0d8202625
prizraksarvar <sarkhas@mail.ru>
2019-03-19 16:36:24 +0500

    Update AutorizedState.ts

--- backend/src/components/autorization/AutorizedState.ts
+++ backend/src/components/autorization/AutorizedState.ts
@@ -1,6 +1,15 @@
 import {RequestState} from "../../core/http/RequestState";
+import {User} from "../../entity/User";
+import {IRequestState} from "../../core/http/IRequestState";
 
 
-export class AutorizedState extends RequestState{
+export class AutorizedState extends RequestState {
+    constructor(
+        public user: User,
+        lastState?: IRequestState
+    ) {
+        super(lastState);
+    }
+
 
 }
