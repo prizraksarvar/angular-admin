58b682bd935bb42ab04cc901dddf74595a450760
prizraksarvar <sarkhas@mail.ru>
2019-03-19 16:35:52 +0500

    Update AutorizationController.ts

--- backend/src/components/autorization/AutorizationController.ts
+++ backend/src/components/autorization/AutorizationController.ts
@@ -5,24 +5,126 @@ import {IResponse} from "../../core/http/IResponse";
 import {INextFunction} from "../../core/http/INextFunction";
 import {NotAutorizedState} from "./NotAutorizedState";
 import {AutorizedState} from "./AutorizedState";
-import {getConnection} from "typeorm";
+import {getConnection, getRepository, MoreThanOrEqual, Raw} from "typeorm";
 import {User} from "../../entity/User";
+import {comparePassword} from "../../core/helpers/PasswordHelper";
+import {AutorizateResponse} from "./AutorizateResponse";
+import {ResponseResultEnum} from "../../core/component/Response";
+import bcrypt from "bcryptjs";
+import {InternalErrorException} from "../../core/component/exceptions/InternalErrorException";
+import {UserNotFoundException} from "./exceptions/UserNotFoundException";
+import {UserSession} from "../../entity/UserSession";
+import {generateToken} from "../../core/helpers/TokenGenerateHelper";
 
 
 export class AutorizationController extends Controller{
     initMiddleware(): void {
         this.registerMiddleware(this.autorizationCheckMiddleware);
-        this.registerRouteMiddleware(RouteMiddlewareTypes.get, "/", this.route)
+        this.registerRouteMiddleware(RouteMiddlewareTypes.get, "/autorization", this.route);
+        this.registerRouteMiddleware(RouteMiddlewareTypes.post, "/autorization/autorizate", this.autorizate);
+        this.registerRouteMiddleware(RouteMiddlewareTypes.post, "/autorization/checkAutorization", this.checkAutorization);
     }
 
     private autorizationCheckMiddleware(request:IRequest, response:IResponse, next:INextFunction) {
-        console.log("request in AutorizationController");
-        if (true) {
-            request.state = new NotAutorizedState(request.state);
-        } else {
-            request.state = new AutorizedState(request.state);
+        if (request.cookies && request.cookies.sessionID) {
+            var sessionID = request.cookies.sessionID;
+            console.log('cookie sessionID found '+sessionID);
+            const sessionRepository = getRepository(UserSession);
+            let date = new Date();
+            date.setHours(date.getHours() + 3);
+            sessionRepository.find({ where: { token: sessionID, updated: MoreThanOrEqual(date) }, relations: ["user"] }).then((sessions:UserSession[])=>{
+                if (sessions.length!=1) {
+                    throw new InternalErrorException('Дубликат сессии или сессия устарела');
+                }
+
+                let session = sessions[0];
+
+                if (!session.user) {
+                    throw new InternalErrorException('Сессия без пользователя, авторизация не требуется');
+                }
+                request.state = new AutorizedState(session.user, request.state);
+            }).catch((err)=>{
+                request.state = new NotAutorizedState(request.state);
+                console.log(err);
+            });
         }
         next();
+    }
+
+    private autorizate(request:IRequest, response:IResponse, next:INextFunction) {
+        const userRepository = getRepository(User);
+        let result = new AutorizateResponse();
+        result.user = null;
+        result.errors = [];
+        result.result = ResponseResultEnum.error;
+        console.log(request.body.login);
+        console.log(request.body.password);
+        userRepository.find({ where: { login: request.body.login } }).then(async (users:User[])=>{
+            console.log(users);
+            if (users.length!=1) {
+                throw new UserNotFoundException('');
+            }
+
+            let user = users[0];
+            let value = await comparePassword(request.body.password,user.password);
+            console.log(value);
+            if (!value) {
+                throw new UserNotFoundException('');
+            }
+            let salt = '';
+            try {
+                salt = await generateToken();
+            } catch (e) {
+                throw new InternalErrorException(e);
+            }
+
+            // TODO: Remove current session token if exist
+            const sessionRepository = getRepository(UserSession);
+            let session = sessionRepository.create();
+            session.token = salt;
+            session.user = user;
+            sessionRepository.save(session).then(()=>{
+                response.cookie('sessionID',salt); //, { maxAge: 1800000, httpOnly: true, path: '/', domain: 'localhost' }
+                result.user = {
+                    id: user.id,
+                    name: user.getFullName(),
+                    login: user.login,
+                    token: 'token'
+                };
+                result.result = ResponseResultEnum.success;
+                response.send(result);
+            });
+        }).catch((err)=>{
+            if (err instanceof UserNotFoundException) {
+                result.errors.push('Не правильный логин или пароль');
+                response.send(result);
+            } else {
+                result.errors.push('Сервер не смог обработать запроса');
+                response.send(result);
+                console.log(err);
+            }
+        });
+    }
+
+    private checkAutorization(request:IRequest, response:IResponse, next:INextFunction) {
+        let result = new AutorizateResponse();
+        result.user = null;
+        result.errors = [];
+        result.result = ResponseResultEnum.error;
+        if (request.state instanceof AutorizedState) {
+            const state:AutorizedState = request.state;
+            result.user = {
+                id: state.user.id,
+                name: state.user.getFullName(),
+                login: state.user.login,
+                token: 'token'
+            };
+            result.result = ResponseResultEnum.success;
+            response.send(result);
+            return;
+        }
+        result.errors.push('Пользователь не авторизован');
+        response.send(result);
     }
 
     private route(request:IRequest, response:IResponse, next:INextFunction) {
