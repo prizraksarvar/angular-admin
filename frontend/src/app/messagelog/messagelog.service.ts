ee7f9240091f742a7767fc5fadb3ecd9430611f4
prizraksarvar <sarkhas@mail.ru>
2019-03-21 16:15:02 +0500

    Update messagelog.service.ts

--- frontend/src/app/messagelog/messagelog.service.ts
+++ frontend/src/app/messagelog/messagelog.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@angular/core';
 
+export enum MessagelogStatus {
+    default,
+    danger,
+    success,
+    warning,
+    info
+}
 @Injectable({ providedIn: 'root' })
 export class MessagelogService {
     messages: string[] = [];
 
-    add(message: string) {
-        this.messages.push(message);
+    add(message: string, status: MessagelogStatus) {
+        let c:string = "text-";
+        switch (status) {
+            case MessagelogStatus.danger: c+='danger';break;
+            case MessagelogStatus.success: c+='success';break;
+            case MessagelogStatus.warning: c+='warning';break;
+            case MessagelogStatus.info: c+='info';break;
+            default: c+='primary';break;
+        }
+        this.messages.push(`<span class="${c}">${message}</span>`);
     }
 
     clear() {
